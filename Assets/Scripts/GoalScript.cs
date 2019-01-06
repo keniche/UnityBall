e5d5db8d78ad0719c113b7fa4ede4ac2b3eef736
keniche <keniche1223@gmail.com>
2019-01-06 17:52:14 +0900

    initial commit

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("Goal");
+        }
+    }
+
+}
---
+++ Assets/Scripts/TimeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeScript : MonoBehaviour {
+
+    public Text time;
+    public static float timeLimit = 30.0f;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLimit = timeLimit - Time.deltaTime;
+        time.text = timeLimit.ToString("f2");
+	}
+}
